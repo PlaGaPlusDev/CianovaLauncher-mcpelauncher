6f32d7e7873d999326de7de77de125ecae7adef0
PlaGaPlusDev <plagaplusdev@outlook.com>
2026-01-05 17:50:07 -0400

    update2.0b

--- cianova-launcher.sh
+++ cianova-launcher.sh
@@ -3,6 +3,7 @@
 # Configurar variables de entorno
 export PATH="/app/bin:$PATH"
 export LD_LIBRARY_PATH="/app/lib:$LD_LIBRARY_PATH"
+export MCPELAUNCHER_DATA_DIR="/app/share/mcpelauncher"
 
 # Directorio de datos del usuario
 DATA_DIR="${XDG_DATA_HOME:-$HOME/.var/app/org.cianova.Launcher/data}/mcpelauncher"
